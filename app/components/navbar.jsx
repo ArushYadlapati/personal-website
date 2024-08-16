9983564f1384106eb20d3f9f4c0b27d37a7b3293
Arush <arushyadlapati.lwsd@gmail.com>
2024-08-16 19:02:40 -0400

    remove smooth scroll, add basic functionality

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -1,45 +1,15 @@
-'use client';
-
 // @flow strict
 import Link from "next/link";
-import { useEffect } from "react";
-import { useRouter } from 'next/navigation';
+
 
 function Navbar() {
-  const router = useRouter();
-
-  useEffect(() => {
-    const handleSmoothScroll = (e) => {
-      e.preventDefault();
-      const href = e.currentTarget.getAttribute('href');
-      const targetId = href.replace(/.*\#/, "");
-
-      if (targetId) {
-        const targetElement = document.getElementById(targetId);
-        if (targetElement) {
-          targetElement.scrollIntoView({ behavior: "smooth" });
-          window.history.pushState({}, '', href);
-        }
-      }
-    };
-
-    const links = document.querySelectorAll('a[href^="/#"]');
-    links.forEach(link => {
-      link.addEventListener("click", handleSmoothScroll);
-    });
-
-    return () => {
-      links.forEach(link => {
-        link.removeEventListener("click", handleSmoothScroll);
-      });
-    };
-  }, []);
-
   return (
       <nav className="bg-transparent">
         <div className="flex items-center justify-between py-5">
           <div className="flex flex-shrink-0 items-center">
-            <Link href="./" className="text-[#16f2b3] text-3xl font-bold">
+            <Link
+                href="/"
+                className=" text-[#16f2b3] text-3xl font-bold">
               arushyadlapati.com
             </Link>
           </div>
@@ -51,24 +21,18 @@ function Navbar() {
               </Link>
             </li>
             <li>
-              <Link className="block px-4 py-2 no-underline outline-none hover:no-underline" href="/#experience">
-                <div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">EXPERIENCE</div>
-              </Link>
+              <Link className="block px-4 py-2 no-underline outline-none hover:no-underline" href="/#experience"><div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">EXPERIENCE</div></Link>
             </li>
             <li>
-              <Link className="block px-4 py-2 no-underline outline-none hover:no-underline" href="/#skills">
-                <div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">SKILLS</div>
-              </Link>
+              <Link className="block px-4 py-2 no-underline outline-none hover:no-underline" href="/#skills"><div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">SKILLS</div></Link>
             </li>
             <li>
-              <Link className="block px-4 py-2 no-underline outline-none hover:no-underline" href="/#projects">
-                <div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">PROJECTS</div>
-              </Link>
+              <Link className="block px-4 py-2 no-underline outline-none hover:no-underline" href="/#projects"><div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">PROJECTS</div></Link>
             </li>
           </ul>
         </div>
       </nav>
   );
-}
+};
 
 export default Navbar;
