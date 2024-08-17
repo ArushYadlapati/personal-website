5c95df7c35836aa47c35258e428b3802cac3a612
Arush <arushyadlapati.lwsd@gmail.com>
2024-08-17 14:13:17 -0400

    Deleting more stuff

--- app/components/homepage/education/index.jsx
+++ app/components/homepage/education/index.jsx
@@ -1,7 +1,7 @@
 // @flow strict
 import Image from "next/image";
 
-import { educations } from "@/utils/data/educations";
+
 import { BsPersonWorkspace } from "react-icons/bs";
 import AnimationLottie from "../../helper/animation-lottie";
 import GlowCard from "../../helper/glow-card";
@@ -80,6 +80,6 @@ function Education() {
       </div>
     </div>
   );
-};
+}
 
 export default Education;
