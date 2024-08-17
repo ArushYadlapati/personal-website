988e0818f0e137a89aa254f7f4f5b295ab478a8c
Arush <arushyadlapati.lwsd@gmail.com>
2024-08-17 13:30:13 -0400

    Added new email (hello@arush.me)

--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -7,35 +7,38 @@ import {BsDiscord, BsGithub} from "react-icons/bs";
 
 function Footer() {
   return (
-    <div className="relative border-t bg-[#0d1224] border-[#353951] text-white">
-      <div className="mx-auto px-6 sm:px-12 lg:max-w-[70rem] xl:max-w-[76rem] 2xl:max-w-[92rem] py-6 lg:py-10">
-        <div className="flex justify-center -z-40">
-          <div className="absolute top-0 h-[1px] w-1/2  bg-gradient-to-r from-transparent via-violet-500 to-transparent"></div>
-        </div>
-        <div className="flex flex-col md:flex-row items-center justify-between">
-          <p className="text-sm">
-            Arush Yadlapati
-          </p>
-          <div className="flex items-center gap-5">
-            <Link
-                href={personalData.github}
-                target='_blank'
-                className="transition-all text-sky-500 hover:scale-125 duration-300"
-            >
-              <BsGithub size={30} />
-            </Link>
-            <Link
-                href={personalData.discord}
-                target='_blank'
-                className="transition-all text-sky-500 hover:scale-125 duration-300"
-            >
-              <BsDiscord size={30} />
-            </Link>
+      <div className="relative border-t bg-[#0d1224] border-[#353951] text-white">
+        <div className="mx-auto px-6 sm:px-12 lg:max-w-[70rem] xl:max-w-[76rem] 2xl:max-w-[92rem] py-6 lg:py-10">
+          <div className="flex justify-center -z-40">
+            <div className="absolute top-0 h-[1px] w-1/2  bg-gradient-to-r from-transparent via-violet-500 to-transparent"></div>
+          </div>
+          <div className="flex flex-col md:flex-row items-center justify-between">
+            <p className="text-sm">
+              Arush Yadlapati
+            </p>
+            <a href="mailto:hello@arush.me" className="text-sm text-sky-500 hover:underline transition-all duration-300">
+              hello@arush.me
+            </a>
+            <div className="flex items-center gap-5">
+              <Link
+                  href={personalData.github}
+                  target='_blank'
+                  className="transition-all text-sky-500 hover:scale-125 duration-300"
+              >
+                <BsGithub size={30} />
+              </Link>
+              <Link
+                  href={personalData.discord}
+                  target='_blank'
+                  className="transition-all text-sky-500 hover:scale-125 duration-300"
+              >
+                <BsDiscord size={30} />
+              </Link>
 
+            </div>
           </div>
         </div>
-      </div>
-    </div >
+      </div >
   );
 };
 
