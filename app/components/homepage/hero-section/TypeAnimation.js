b0bf931357fdcaac22183971fe9f69c682438f08
Arush <arushyadlapati.lwsd@gmail.com>
2024-08-17 16:48:19 -0400

    Update TypeAnimation.js

--- app/components/homepage/hero-section/TypeAnimation.js
+++ app/components/homepage/hero-section/TypeAnimation.js
@@ -9,7 +9,7 @@ const TypeAnimation = () => {
     const [typingSpeed, setTypingSpeed] = useState(150);
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
-    const words = ['programmer', 'sophomore', 'roboticist', 'mentor'];
+    const words = ['programmer', 'sophomore', 'roboticist'];
 
     useEffect(() => {
         const handleTyping = () => {
