4fb9238804020c0d50849c72436652ac1a7c7227
Arush <arushyadlapati.lwsd@gmail.com>
2024-08-21 13:11:33 -0400

    testing mobile view

--- app/components/homepage/projects/index.jsx
+++ app/components/homepage/projects/index.jsx
@@ -1,21 +1,26 @@
-import { projectsData } from '@/utils/data/projects-data';
+import {projectsData} from '@/utils/data/projects-data';
 import ProjectCard from './project-card';
 import Image from "next/image";
+import '/app/css/mobile.css';
 
 const Projects = () => {
 
-  return (
-    <div id='projects' className="relative z-50  my-12 lg:my-24">
-      <div className="sticky top-10">
-        <div className="w-[600px] h-[10px] bg-violet-100 rounded-full absolute -top-3 left-0 translate-x-1/2 filter blur-3xl  opacity-30"></div>
-        <div className="flex items-center justify-start relative">
-          <span className="bg-[#1a1443] absolute left-0  w-fit text-white px-5 py-3 text-xl rounded-md">
+    return (<div id='projects' className="relative z-50  my-12 lg:my-24">
+        <div className="sticky top-10">
+            <div
+                className="w-[600px] h-[10px] bg-violet-100 rounded-full absolute -top-3 left-0 translate-x-1/2 filter blur-3xl  opacity-30"></div>
 
-            PROJECTS
+            <div className="flex justify-center my-5 lg:py-8">
+                <div className="flex  items-center">
+                    <span className="w-24 h-[2px] bg-[#1a1443]"></span>
+                    <span className="bg-[#1a1443] w-fit text-white p-2 px-5 text-xl rounded-md">
+            Projects
           </span>
-          <span className="w-full h-[2px] bg-[#1a1443]"></span>
+                    <span className="w-24 h-[2px] bg-[#1a1443]"></span>
+                </div>
+            </div>
+
         </div>
-      </div>
         <Image
             src="./section.svg"
             alt="Hero"
@@ -25,31 +30,28 @@ const Projects = () => {
         />
         <div className="pt-24">
 
-        <div className="flex flex-col gap-6">
-          {projectsData.slice(0, 4).map((project, index) => (
-            <div
-              id={`sticky-card-${index + 1}`}
-              key={index}
-              className="sticky-card w-full mx-auto max-w-2xl sticky"
-            >
-              <div className="box-border flex items-center justify-center rounded shadow-[0_0_30px_0_rgba(0,0,0,0.3)] transition-all duration-[0.5s]">
-                <ProjectCard project={project} />
-                  <Image
-                      src="./blur-23.svg"
-                      alt="Hero"
-                      width={1080}
-                      height={200}
-                      className="absolute bottom-0 opacity-80"
-                  />
-              </div>
+            <div className="flex flex-col gap-6">
+                {projectsData.slice(0, 4).map((project, index) => (<div
+                    id={`sticky-card-${index + 1}`}
+                    key={index}
+                    className="sticky-card w-full mx-auto max-w-2xl sticky"
+                >
+                    <div
+                        className="box-border flex items-center justify-center rounded shadow-[0_0_30px_0_rgba(0,0,0,0.3)] transition-all duration-[0.5s] w-[1000px] flex-mobile"> {/* Add flex-mobile class */}
+                        <ProjectCard project={project}/>
+                        <Image
+                            src="./blur-23.svg"
+                            alt="Hero"
+                            width={1080}
+                            height={200}
+                            className="absolute bottom-0 opacity-80"
+                        />
+                    </div>
+                </div>))}
             </div>
-          ))}
         </div>
-      </div>
-    </div>
-  );
+    </div>);
 };
-
 
 
 export default Projects;
--- app/components/homepage/projects/project-card.jsx
+++ app/components/homepage/projects/project-card.jsx
@@ -1,104 +1,119 @@
 import * as React from 'react';
 import Image from "next/image";
 
-function ProjectCard({ project }) {
-  return (
-      <div className="from-[#0d1224] border-[#1b2c68a0] relative rounded-lg border bg-gradient-to-r to-[#0a0d37] w-full flex flex-col lg:flex-row">
-        <div className="flex-1">
-          <div className="flex flex-row">
-            <div className="h-[1px] w-full bg-gradient-to-r from-transparent via-pink-500 to-violet-600"></div>
-            <div className="h-[1px] w-full bg-gradient-to-r from-violet-600 to-transparent"></div>
-          </div>
-          <div className="px-4 lg:px-8 py-3 lg:py-5 relative">
-            <div className="flex flex-row space-x-1 lg:space-x-2 absolute top-1/2 -translate-y-1/2">
-              <div className="h-2 w-2 lg:h-3 lg:w-3 rounded-full bg-red-400"></div>
-              <div className="h-2 w-2 lg:h-3 lg:w-3 rounded-full bg-orange-400"></div>
-              <div className="h-2 w-2 lg:h-3 lg:w-3 rounded-full bg-green-200"></div>
-            </div>
-            <p className="text-center ml-3 text-[#16f2b3] text-base lg:text-xl">
-              {project.name}
-            </p>
-          </div>
-          <div className="overflow-hidden border-t-[2px] border-indigo-900 px-4 lg:px-8 py-4 lg:py-8">
-            <code className="font-mono text-xs md:text-sm lg:text-base">
-              <div className="blink">
-                <span className="mr-2 text-pink-500">const</span>
-                <span className="mr-2 text-white">project</span>
-                <span className="mr-2 text-pink-500">=</span>
-                <span className="text-gray-400">{'{'}</span>
-              </div>
-              <div>
-                <span className="ml-4 lg:ml-8 mr-2 text-white">name:</span>
-                <span className="text-gray-400">{`'`}</span>
-                <span className="text-amber-300">{project.name}</span>
-                <span className="text-gray-400">{`',`}</span>
-              </div>
+function ProjectCard({project}) {
+    return (
+        <div
+            className="order-1 lg:order-2 from-[#0d1224] border-[#1b2c68a0] relative rounded-lg border bg-gradient-to-r to-[#0a0d37] w-[1000px]">
+            <div className="flex-1">
+                <div className="flex flex-row">
+                    <div className="h-[1px] w-full bg-gradient-to-r from-transparent via-pink-500 to-violet-600"></div>
+                    <div className="h-[1px] w-full bg-gradient-to-r from-violet-600 to-transparent"></div>
+                </div>
+                <div className="px-4 lg:px-8 py-3 lg:py-5 relative">
+                    <div className="flex flex-row space-x-1 lg:space-x-2 absolute top-1/2 -translate-y-1/2">
+                        <div className="h-2 w-2 lg:h-3 lg:w-3 rounded-full bg-red-400"></div>
+                        <div className="h-2 w-2 lg:h-3 lg:w-3 rounded-full bg-orange-400"></div>
+                        <div className="h-2 w-2 lg:h-3 lg:w-3 rounded-full bg-green-200"></div>
+                    </div>
+                    <p className="text-center ml-3 text-[#16f2b3] text-base lg:text-xl">
+                        {project.name}
+                    </p>
+                </div>
+                <div className="overflow-hidden border-t-[2px] border-indigo-900 px-4 lg:px-8 py-4 lg:py-8">
+                    <code className="font-mono text-xs md:text-sm lg:text-base">
+                        <div className="blink">
+                            <span className="mr-2 text-pink-500">const</span>
+                            <span className="mr-2 text-white">project</span>
+                            <span className="mr-2 text-pink-500">=</span>
+                            <span className="text-gray-400">{'{'}</span>
+                        </div>
+                        <div>
+                            <span className="ml-4 lg:ml-8 mr-2 text-white">name:</span>
+                            <span className="text-gray-400">{`'`}</span>
+                            <span className="text-amber-300">{project.name}</span>
+                            <span className="text-gray-400">{`',`}</span>
+                        </div>
 
-              <div className="relative group">
-                <span className="ml-4 lg:ml-8 mr-2 text-white">link:</span>
-                <span className="text-gray-400">&#39;</span>
-                <span className="text-[#1C8AB5] underline group-hover:text-[#19F2B3] transition-colors duration-300">
+                        <div className="relative group">
+                            <span className="ml-4 lg:ml-8 mr-2 text-white">link:</span>
+                            <span className="text-gray-400">&#39;</span>
+                            <span
+                                className="text-[#1C8AB5] underline group-hover:text-[#19F2B3] transition-colors duration-300">
                 {project.link}
               </span>
-                <span className="text-gray-400">&#39;</span>
-                <a href={project.link} target="_blank" rel="noopener noreferrer"
-                   className="absolute inset-0 z-10 cursor-pointer" aria-label={`Visit ${project.name} project`}>
-                  <span className="sr-only">Visit project link</span>
-                </a>
-                <span className="text-gray-400">,</span>
-              </div>
+                            <span className="text-gray-400">&#39;</span>
+                            <a href={project.link} target="_blank" rel="noopener noreferrer"
+                               className="absolute inset-0 z-10 cursor-pointer"
+                               aria-label={`Visit ${project.name} project`}>
+                                <span className="sr-only">Visit project link</span>
+                            </a>
+                            <span className="text-gray-400">,</span>
+                        </div>
 
-              <div>
-                <span className="ml-4 lg:ml-8 mr-2 text-white">date:</span>
-                <span className="text-gray-400">{`'`}</span>
-                <span className="text-amber-300">{project.date}</span>
-                <span className="text-gray-400">{`',`}</span>
-              </div>
+                        <div>
+                            <span className="ml-4 lg:ml-8 mr-2 text-white">date:</span>
+                            <span className="text-gray-400">{`'`}</span>
+                            <span className="text-amber-300">{project.date}</span>
+                            <span className="text-gray-400">{`',`}</span>
+                        </div>
 
-              <div className="ml-4 lg:ml-8 mr-2">
-                <span className=" text-white">tools:</span>
-                <span className="text-gray-400">{` ['`}</span>
+                        <div className="ml-4 lg:ml-8 mr-2">
+                            <span className=" text-white">tools:</span>
+                            <span className="text-gray-400">{` ['`}</span>
 
-                {
-                  project.tools.map((tag, i) => (
-                      <React.Fragment key={i}>
-                        <span className="text-amber-300">{tag}</span>
-                        {
-                            project.tools.length - 1 !== i &&
-                            <span className="text-gray-400">{`', '`}</span>
-                        }
-                      </React.Fragment>
-                  ))
-                }
-                <span className="text-gray-400">{"'],"}</span>
-              </div>
-              <div>
-                <span className="ml-4 lg:ml-8 mr-2 text-white">my_role:</span>
-                <span className="text-orange-400">{project.role}</span>
-                <span className="text-gray-400">,</span>
-              </div>
-              <div className="ml-4 lg:ml-8 mr-2">
-                <span className="text-white">description:</span>
-                <span className="text-cyan-400">{' ' + project.description}</span>
-                <span className="text-gray-400"></span>
-              </div>
-              <div>
-                <span className="text-gray-400">{`};`}</span>
-              </div>
-            </code>
-          </div>
+                            {
+                                project.tools.map((tag, i) => (
+                                    <React.Fragment key={i}>
+                                        <span className="text-amber-300">{tag}</span>
+                                        {
+                                            project.tools.length - 1 !== i &&
+                                            <span className="text-gray-400">{`', '`}</span>
+                                        }
+                                    </React.Fragment>
+                                ))
+                            }
+                            <span className="text-gray-400">{"'],"}</span>
+                        </div>
+                        <div>
+                            <span className="ml-4 lg:ml-8 mr-2 text-white">my_role:</span>
+                            <span className="text-orange-400">{project.role}</span>
+                            <span className="text-gray-400">,</span>
+                        </div>
+                        <div className="ml-4 lg:ml-8 mr-2">
+                            <span className="text-white">description:</span>
+                            <span className="text-cyan-400">{' ' + project.description}</span>
+                            <span className="text-gray-400"></span>
+                        </div>
+                        <div>
+                            <span className="text-gray-400">{`};`}</span>
+                        </div>
+                    </code>
+                </div>
+            </div>
+
+            <div className="mt-4 lg:mt-0 lg:ml-4 flex-shrink-0 w-full lg:w-auto">
+                <div className="p-4">
+                    <Image
+                        src={project.image}
+                        alt={project.name}
+                        width={1080}
+                        height={200}
+                        className="w-full h-full object-cover rounded-lg"
+                        style={{
+                            maxWidth: '75%',
+                            maxHeight: '75%',
+                            objectFit: 'cover',
+                            borderRadius: '10px',
+                            display: 'block'
+                        }}
+                    />
+                </div>
+            </div>
+
+
         </div>
-        {/*<div className="mt-4 lg:mt-0 lg:ml-4 flex-shrink-0 w-full lg:w-auto">*/}
-        {/*  <Image*/}
-        {/*      src={project.image}*/}
-        {/*      alt={project.name}*/}
-        {/*      width={1080}*/}
-        {/*      height={200}*/}
-        {/*      className="w-full h-full object-cover rounded-lg"*/}
-        {/*  />*/}
-        {/*</div>*/}
-      </div>
-  );
+    );
 }
 
 export default ProjectCard;
